2ac0726ef26fb2f5398d7de7b8eaf09991c3dabe
BuildTools <unconfigured@null.spigotmc.org>
2022-07-31 16:38:31 +0100

    Plugin initialisation

---
+++ src/main/java/tk/utnetwork/utnetworklobby/UTNetworkLobby.java
@@ -0,0 +1,17 @@
+package tk.utnetwork.utnetworklobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UTNetworkLobby extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
